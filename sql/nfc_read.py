d7442b213f2a1241b0ee7624122dc6327f0d0adc
LimJaeYong <101688100+LimJaeYong@users.noreply.github.com>
2023-08-02 02:36:59 +0900

    Update nfc_read.py

--- sql/nfc_read.py
+++ sql/nfc_read.py
@@ -1,35 +1,33 @@
-import time
 import board
 import busio
 import adafruit_pn532.i2c as PN532
 
-i2c, pn532 = None, None
-returnUid = ''
 
 
-def __init__(self):
-    self.i2c = busio.I2C(board.SCL, board.SDA)
+class nfc:
+    def __init__(self):
+        self.i2c = busio.I2C(board.SCL, board.SDA)
 
-    self.pn532 = PN532.PN532_I2C(self.i2c, debug=False)
+        self.pn532 = PN532.PN532_I2C(self.i2c, debug=False)
 
-    self.pn532.SAM_configuration()
+        self.pn532.SAM_configuration()
 
 
-def run():
-    print("Waiting for RFID/NFC card.")
+    def run(self):
+        print("Waiting for RFID/NFC card.")
+        returnUid = ''
 
-    while True:
-        uid = pn532.read_passive_target(timeout=0.5)
+        while True:
+            self.uid = self.pn532.read_passive_target(timeout=0.5)
 
-        if uid is not None:
-            print("Found card with UID: ", [hex(i) for i in uid])
-            print("Found card with UID (string): ",
-                  "".join([chr(i) for i in uid]))
-            # Convert hex to string
-            returnUid.join([chr(i) for i in uid])
-        else:
-            break
+            if self.uid is not None:
+                #print(self.uid)
+                #print("Found card with UID: ", [hex(i) for i in self.uid])
+                returnUid = ''.join([chr(i) for i in self.uid])
+                print("Found card with UID: ", returnUid)               
+                
+                return returnUid
+            
 
-        print(".")
+            print(".")
 
-    return returnUid
