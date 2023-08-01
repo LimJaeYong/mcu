a44f1a4cebf54ca6813d0e9972179c8e33b5288c
LimJaeYong <101688100+LimJaeYong@users.noreply.github.com>
2023-08-02 00:49:41 +0900

    Update mainQueryControl copy.py

--- sql/mainQueryControl copy.py
+++ sql/mainQueryControl copy.py
@@ -28,19 +28,27 @@ def Connect_DB():
     except mc.Error as e:
         print("Connection error: ", e)
 
+
+# insert query into database
+
+def insertQ(name, uid, room):
+    insertQuery = "INSERT INTO usertable (user_name, nfc_id, room) VALUE('" + \
+        name+"','"+uid+"','"+room+"');"
+    executeQ(connection, insertQuery)
+
 # select query from usertable to check if uid is vaild
 
 
 def selectQ(uid):
-    selectQuery = "SELECT IF(nfc_id ='"+uid+"', 'Y', 'N') from usertable;"
+    selectQuery = "SELECT IF(nfc_id ="+uid+", 'Y', 'N') from usertable;"
     result = executeQ(connection, selectQuery)
     print(result[0][0])
     if result[0][0] == 'Y':
         print("Access granted.")
-        name = "SELECT name FROM usertable WHERE uid = '"+uid+"';"
+        name = "SELECT name FROM usertable WHERE uid = "+uid+";"
         status = "TRUE"
         # if this user was already granted and pass the gate
-        if executeQ(connection, "SELECT pass_gate from usertable WHERE nfc_id ='"+uid+"';")[0][0]:
+        if executeQ(connection, "SELECT pass_gate from usertable WHERE nfc_id ="+uid+";")[0][0]:
             # Switch the pass_gate status and notify that this user is now Exit the gate
             status = "FALSE"
             print("Good Bye.")
@@ -80,6 +88,7 @@ def executeQ(connection, query):
 
 if __name__ == "__main__":
     connection = Connect_DB()
+    insertQ("Lim Jae Yong", nfc.run(), "testroom")
 
     while (True):
         uid = nfc.run()
@@ -89,14 +98,10 @@ if __name__ == "__main__":
             print("Please try again.")
             continue
 
-
-
-
-
         # Test
-        #selectQ("0000")
+        # selectQ("0000")
         # Test: Not passing gate yet
-        #selectQ("1111")
+        # selectQ("1111")
         # Test: Already pass the gate and ready to Exit the gate
-        #selectQ("1111")
-        #break
+        # selectQ("1111")
+        # break
