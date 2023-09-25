9a7e7158937a0b20d2a39b6e8becc1d0a2af2ccf
LimJaeYong <101688100+LimJaeYong@users.noreply.github.com>
2023-09-25 23:57:00 +0900

    Update buzzer.py

--- sql/buzzer.py
+++ sql/buzzer.py
@@ -1,14 +1,38 @@
 import RPi.GPIO as GPIO
 import time
 
-buzzer = 17
+buzzer = 4
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(buzzer, GPIO.OUT)
 GPIO.setwarnings(False)
 
-pwm = GPIO.PWM(buzzer, 262)
-pwm.start(50.0)
-time.sleep(1.5)
+pwm = GPIO.PWM(buzzer, 1.0)
 
-pwm.stop()
+
+scale = [262, 294, 330, 349, 392, 440, 494, 523]
+
+def y():
+    pwm.start(50.0)
+    
+    pwm.ChangerFrequency(scale[0])
+    time.sleep(1.0)
+    
+    pwm.ChangerFrequency(scale[2])
+    time.sleep(1.0)
+    
+    pwm.ChangerFrequency(scale[4])
+    time.sleep(1.0)
+    
+    pwm.ChangerFrequency(scale[7])
+    time.sleep(1.0)
+    
+    pwm.stop()
+    
+def n():
+    pwm.ChangerFrequency(311)
+    time.sleep(1.0)
+    
+    pwm.stop()
+
 GPIO.cleanup()
